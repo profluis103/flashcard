ab8def27583896fe69702ecc0715e8303f7c1024
profluis103 <luisjesus@prof.educacao.sp.gov.br>
2024-11-22 08:10:44 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'palmeras tem mundial?',
+    'Nao'
+)
+
+criaCartao(
+    'futebol',
+    'O  vini jr e o melhor do mundo?',
+    'sim'
+)
+
+criaCartao(
+    'futebol',
+    'O flamengo e ajudado pela cbf',
+    'sim'
+)
+
+criaCartao(
+    'futebol',
+    'O corinthians se salva do rebaixamento',
+    'Sim'
+)
